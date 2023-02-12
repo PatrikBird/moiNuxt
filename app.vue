864e5d445f1e5a7e8137cd4ee9c5b11c493cbc74
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2023-02-12 15:35:55 +0900

    Inital commit

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <NuxtWelcome />
+    <div>
+      <ColorModeSwitch />
+      <Icon name="mdi:check" class="text-green-500 dark:text-blue-500" />
+    </div>
+  </div>
+</template>
